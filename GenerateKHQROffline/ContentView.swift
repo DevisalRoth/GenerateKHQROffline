397df98dd484307d62e09b9712bfd4abab97fa96
Visalroth <chouvisalroth9999@gmail.com>
2025-12-25 10:48:47 +0700

    Initial Commit

---
+++ GenerateKHQROffline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GenerateKHQROffline
+//
+//  Created by Visalroth on 25/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GenerateKHQROffline/GenerateKHQROfflineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerateKHQROfflineApp.swift
+//  GenerateKHQROffline
+//
+//  Created by Visalroth on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GenerateKHQROfflineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
